2aa12b6bd5321dec4e9e43706759f089930ab6ab
Juan Sebastián Vargas <tu-correo@ejemplo.com>
2026-06-02 22:33:35 -0400

    actualizacion tag modulo redes

--- variables.tf
+++ variables.tf
@@ -1,19 +1,52 @@
-﻿variable "vpc_cidr" {
-  description = "CIDR block para la VPC"
+﻿# Variables para el módulo redes
+variable "vpc_cidr" {
+  description = "CIDR block de la VPC"
   type        = string
 }
 
-variable "bucket_name" {
-  description = "Nombre del bucket S3"
+variable "vpc_name" {
+  description = "Nombre de la VPC"
   type        = string
 }
 
-variable "instance_type" {
-  description = "Tipo de instancia EC2"
+variable "public_subnets" {
+  description = "Lista de subnets públicas"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "Availability Zones"
+  type        = list(string)
+}
+
+variable "sg_name" {
+  description = "Nombre del Security Group"
   type        = string
 }
 
-variable "ami_id" {
-  description = "AMI a utilizar para la instancia EC2"
+variable "allowed_ips" {
+  description = "Lista de IPs permitidas para acceso"
+  type        = list(string)
+}
+
+# Variables para el módulo computo
+variable "instance_name" {
+  description = "Nombre de la instancia EC2"
+  type        = string
+}
+
+# Variables para el módulo almacenamiento
+variable "bucket_acl" {
+  description = "ACL del bucket S3"
+  type        = string
+}
+
+variable "environment" {
+  description = "Entorno (dev, test, prod)"
+  type        = string
+}
+
+variable "versioning_status" {
+  description = "Estado del versionado del bucket"
   type        = string
 }
