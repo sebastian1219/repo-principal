dadfdbe0bed7399c0aa09a7a4ef47746ab1ab042
VICENTE0777 <vic.alarcona@duocuc.cl>
2026-06-03 00:15:22 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,10 @@
+# Token para clonar módulos privados
+variable "github_token" {
+  description = "GitHub token for private module access"
+  type        = string
+}
+
+# Redes
 variable "vpc_cidr" { type = string }
 variable "vpc_name" { type = string }
 variable "public_subnets" { type = list(string) }
@@ -5,10 +12,12 @@ variable "azs" { type = list(string) }
 variable "sg_name" { type = string }
 variable "allowed_ips" { type = list(string) }
 
+# Cómputo
 variable "ami_id" { type = string }
 variable "instance_type" { type = string }
 variable "instance_name" { type = string }
 
+# Almacenamiento
 variable "bucket_name" { type = string }
 variable "bucket_acl" { type = string }
 variable "environment" { type = string }
